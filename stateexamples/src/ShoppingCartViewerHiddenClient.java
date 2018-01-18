bfac6334fed7871247a335895a6100f7fe61ecd6
kgary <kgary@asu.edu>
2018-01-17 17:55:23 -0700

    Updated state examples, added Shopping Cart version with HttpSession

--- stateexamples/src/ShoppingCartViewerHiddenClient.java
+++ stateexamples/src/ShoppingCartViewerHiddenClient.java
@@ -7,41 +7,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
-Example taken from http://www.java2s.com/Code/Java/Servlets
-and then augmented to provide multipage behavior
+   Example taken from http://www.java2s.com/Code/Java/Servlets
+   and then augmented to provide multipage behavior
 */
 @SuppressWarnings("serial")
 public class ShoppingCartViewerHiddenClient extends HttpServlet {
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-	    String[] items = req.getParameterValues("item");
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	String[] items = req.getParameterValues("item");
 
-	    res.setContentType("text/html");
-	    PrintWriter out = res.getWriter();
-	    out.println("<HEAD><TITLE>Hidden Shopping Cart POST</TITLE></HEAD>");
-	    out.println("<BODY>");
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter();
+	out.println("<HEAD><TITLE>Hidden Shopping Cart POST</TITLE></HEAD>");
+	out.println("<BODY>");
 	    
-	    // need to convert back to a proper delimited URL param
-		String requestURI = req.getRequestURI();
+	// need to convert back to a proper delimited URL param
+	String requestURI = req.getRequestURI();
 
-	    SessionExampleUtils.printClientForm(out, SessionExampleUtils.computeClientPost(items, out, req), 
-	    		requestURI, true);
-	    out.println("</BODY></HTML>");
-	}
+	String newItem = req.getParameter("newitem");
+	String action = req.getParameter("action");
+
+	SessionExampleUtils.printClientForm(out, SessionExampleUtils.computeClientPost(items, out, newItem, action), 
+					    requestURI, true);
+	out.println("</BODY></HTML>");
+    }
 	
-  public void doGet(HttpServletRequest req, HttpServletResponse res)
-                               throws ServletException, IOException {
-    res.setContentType("text/html");
-    PrintWriter out = res.getWriter();
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+	throws ServletException, IOException {
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter();
 
-    out.println("<HEAD><TITLE>Hidden Shopping Cart GET</TITLE></HEAD>");
-    out.println("<BODY>");
+	out.println("<HEAD><TITLE>Hidden Shopping Cart GET</TITLE></HEAD>");
+	out.println("<BODY>");
 
-    // Cart items are passed in as the item parameter.
-    String[] items = req.getParameterValues("item");
-    String requestURI = req.getRequestURI();
+	// Cart items are passed in as the item parameter.
+	String[] items = req.getParameterValues("item");
+	String requestURI = req.getRequestURI();
 
-    SessionExampleUtils.printClientForm(out, items, requestURI, true);
-    out.println("</BODY></HTML>");
-  }
+	SessionExampleUtils.printClientForm(out, items, requestURI, true);
+	out.println("</BODY></HTML>");
+    }
 }
--- stateexamples/src/ShoppingCartViewerRewriteClient.java
+++ stateexamples/src/ShoppingCartViewerRewriteClient.java
@@ -7,61 +7,64 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
-Example taken from http://www.java2s.com/Code/Java/Servlets
-and then augmented to provide multipage behavior
+   Example taken from http://www.java2s.com/Code/Java/Servlets
+   and then augmented to provide multipage behavior
 */
 @SuppressWarnings("serial")
 public class ShoppingCartViewerRewriteClient extends HttpServlet {
 
-	private static final String URL_DELIMITER = ":";
+    private static final String URL_DELIMITER = ":";
 
-	private static String[] processQueryString(HttpServletRequest req, String param, String delim) {
-		String cItems = req.getParameter(param);
-		if (cItems != null && !cItems.isEmpty()) {
-			return cItems.split(delim);
-		}
-	    return null;
+    private static String[] processQueryString(HttpServletRequest req, String param, String delim) {
+	String cItems = req.getParameter(param);
+	if (cItems != null && !cItems.isEmpty()) {
+	    return cItems.split(delim);
 	}
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-	    String[] items = processQueryString(req, "items", URL_DELIMITER);
-	    
-	    res.setContentType("text/html");
-	    PrintWriter out = res.getWriter();
-	    out.println("<HEAD><TITLE>Rewrite Client Shopping Cart POST</TITLE></HEAD>");
-	    out.println("<BODY>");
-	    
-	    // need to convert back to a proper delimited URL param
-		String requestURI = req.getRequestURI();
-		String[] newItems = SessionExampleUtils.computeClientPost(items, out, req);
-	    if (newItems != null && newItems.length > 0) {
-	    	StringBuffer sb = new StringBuffer();
-	    	for (String s : newItems) {
-	    		sb.append(s + URL_DELIMITER);
-	    	}
-	    	String s = sb.toString();
-	    	requestURI = res.encodeURL(requestURI + "?items=" + s.substring(0, s.length()-1));
-	    }
-	    SessionExampleUtils.printClientForm(out, newItems, requestURI, false);
-	    out.println("</BODY></HTML>");
-	}
-	
-  public void doGet(HttpServletRequest req, HttpServletResponse res)
-                               throws ServletException, IOException {
-    res.setContentType("text/html");
-    PrintWriter out = res.getWriter();
-
-    out.println("<HEAD><TITLE>Rewrite Shopping Cart GET</TITLE></HEAD>");
-    out.println("<BODY>");
-
-    // Cart items are passed in as the item parameter.
-    String[] items = processQueryString(req, "items", URL_DELIMITER);
-    String requestURI = req.getRequestURI();
-    if (items != null && items.length > 0) {
-    	requestURI = requestURI + "?items=" + req.getParameter("items");
+	return null;
     }
+	
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	String[] items = processQueryString(req, "items", URL_DELIMITER);
+	    
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter();
+	out.println("<HEAD><TITLE>Rewrite Client Shopping Cart POST</TITLE></HEAD>");
+	out.println("<BODY>");
+	    
+	// need to convert back to a proper delimited URL param
+	String requestURI = req.getRequestURI();
+	String newItem = req.getParameter("newitem");
+	String action = req.getParameter("action");
 
-    SessionExampleUtils.printClientForm(out, items, requestURI, false);
-    out.println("</BODY></HTML>");
-  }
+	String[] newItems = SessionExampleUtils.computeClientPost(items, out, newItem, action);
+	if (newItems != null && newItems.length > 0) {
+	    StringBuffer sb = new StringBuffer();
+	    for (String s : newItems) {
+		sb.append(s + URL_DELIMITER);
+	    }
+	    String s = sb.toString();
+	    requestURI = res.encodeURL(requestURI + "?items=" + s.substring(0, s.length()-1));
+	}
+	SessionExampleUtils.printClientForm(out, newItems, requestURI, false);
+	out.println("</BODY></HTML>");
+    }
+	
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+	throws ServletException, IOException {
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter();
+
+	out.println("<HEAD><TITLE>Rewrite Shopping Cart GET</TITLE></HEAD>");
+	out.println("<BODY>");
+
+	// Cart items are passed in as the item parameter.
+	String[] items = processQueryString(req, "items", URL_DELIMITER);
+	String requestURI = req.getRequestURI();
+	if (items != null && items.length > 0) {
+	    requestURI = requestURI + "?items=" + req.getParameter("items");
+	}
+
+	SessionExampleUtils.printClientForm(out, items, requestURI, false);
+	out.println("</BODY></HTML>");
+    }
 }
