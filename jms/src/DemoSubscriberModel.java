c3c649a3686dcb2b162fd3dd0b55d99582030383
kgary <kgary@asu.edu>
2019-03-28 09:01:03 -0700

    Took out verbose logging and debugging to screen

--- jms/src/DemoSubscriberModel.java
+++ jms/src/DemoSubscriberModel.java
@@ -12,30 +12,27 @@ public class DemoSubscriberModel implements javax.jms.MessageListener {
     /* Establish JMS subscriber */
     public DemoSubscriberModel(String topicName, String username,
 			       String password) throws Exception {
-		System.out.println("HERE 0");
+
 	// Obtain a JNDI connection
 	InitialContext jndi = new InitialContext();
 	// Look up a JMS connection factory
-		System.out.println("HERE 00");
+
 	TopicConnectionFactory conFactory = (TopicConnectionFactory)jndi.lookup("topicConnectionFactry");
 	// Create a JMS connection
-		System.out.println("HERE 000");
 	connection = conFactory.createTopicConnection();
-		System.out.println("HERE 0000");
 	connection.setClientID("ser422");  // this is normally done by configuration not programmatically
-	System.out.println("HERE 1");
+
 	// Look up a JMS topic
 	Topic chatTopic = (Topic) jndi.lookup(topicName);
-		System.out.println("HERE 2");
+
 	TopicSession subSession = connection.createTopicSession(false,
 								Session.AUTO_ACKNOWLEDGE);
-		System.out.println("HERE 3");
+
 	TopicSubscriber subscriber = subSession.createDurableSubscriber(chatTopic, "DemoSubscriberModel");
 	// Set a JMS message listener
 	// Start the JMS connection; allows messages to be delivered
-		System.out.println("HERE 4");
 	connection.start();
-	System.out.println("HERE 5");
+
 	//subscriber.setMessageListener(this);
 	int count=0;
         while (count < 10) {
@@ -64,7 +61,7 @@ public class DemoSubscriberModel implements javax.jms.MessageListener {
 
         public static void main(String[] args) {
 	// uncomment this line for verbose logging to the screen
-	BasicConfigurator.configure();
+	// BasicConfigurator.configure();
 
 	try {
 	    if (args.length != 3)
