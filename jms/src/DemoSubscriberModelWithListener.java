e7bbdddf76c46d2ae1cd32b6c275fde24d3c52d5
kgary <kgary@asu.edu>
2020-06-12 01:31:34 -0700

    Added Subscriber with Listener

--- jms/src/DemoSubscriberModelWithListener.java
+++ jms/src/DemoSubscriberModelWithListener.java
@@ -4,12 +4,12 @@ import org.apache.log4j.BasicConfigurator;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class DemoSubscriberModel {
+public class DemoSubscriberModelWithListener implements javax.jms.MessageListener {
     private TopicSession pubSession;
     private TopicConnection connection;
 
     /* Establish JMS subscriber */
-    public DemoSubscriberModel(String topicName, String clientName, String username, String password)
+    public DemoSubscriberModelWithListener(String topicName, String clientName, String username, String password)
 	throws Exception {
 	// Obtain a JNDI connection
 	InitialContext jndi = new InitialContext();
@@ -22,17 +22,25 @@ public class DemoSubscriberModel {
 	Topic chatTopic = (Topic) jndi.lookup(topicName);
 	TopicSession subSession = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
 	TopicSubscriber subscriber = subSession.createDurableSubscriber(chatTopic, "DemoSubscriberModel");
+
+	subscriber.setMessageListener(this);  // so we will use onMessage
+	
 	// Start the JMS connection; allows messages to be delivered
 	connection.start();
-	// Listen for 10 messages
-	int count=0;
-        while (count < 10) {
-            System.out.println("waiting for message...");
-            TextMessage msg = (TextMessage) subscriber.receive();
-            System.out.println(++count + "Received message: " + msg.getText());
-	}
-	connection.close();
     }
+
+   public void onMessage(Message message) {
+        try {
+            if (message instanceof TextMessage) {
+                TextMessage txtMessage = (TextMessage) message;
+                System.out.println("Message received: " + txtMessage.getText());
+            } else {
+                System.out.println("Invalid message received.");
+            }
+        } catch (JMSException e1) {
+	    e1.printStackTrace();
+	}
+   }
     
     public static void main(String[] args) {
 	// uncomment this line for verbose logging to the screen
@@ -41,7 +49,8 @@ public class DemoSubscriberModel {
 	    if (args.length != 4)
 		System.out.println("Please Provide the topic name,unique client id, username,password!");
 
-	    DemoSubscriberModel demo = new DemoSubscriberModel(args[0], args[1], args[2], args[3]);
+	    DemoSubscriberModelWithListener demo =
+		new DemoSubscriberModelWithListener(args[0], args[1], args[2], args[3]);
 	    BufferedReader commandLine = new java.io.BufferedReader(new InputStreamReader(System.in));
 	    
 	    // closes the connection and exit the system when 'exit' enters in
